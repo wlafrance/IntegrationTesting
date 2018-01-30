5347abf7941de25f36bb67d1843aecd3f9f1b667
Warren LaFrance <warrenlafrance@outlook.com>
2018-01-30 11:21:34 -0600

    Added compoonents and routes
    Added components and routes for testing

--- integration-testing-demo/src/app/app.component.spec.ts
+++ integration-testing-demo/src/app/app.component.spec.ts
@@ -1,6 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed,ComponentFixture ,async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing'; 
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { AppComponent } from './app.component';
+
+
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,6 +17,13 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges(); 
+  });
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
