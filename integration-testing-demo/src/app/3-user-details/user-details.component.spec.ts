42101a6c1da26150f0d50aa010e5e3e0a18b32e9
Warren LaFrance <warrenlafrance@outlook.com>
2018-02-01 14:12:44 -0600

    commiting

--- integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
+++ integration-testing-demo/src/app/3-user-details/user-details.component.spec.ts
@@ -2,18 +2,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { UserDetailsComponent } from './user-details.component';
 
+class RouterStub {
+  // DUMMY LIGHT WEIGHT ROUTER CLASS IN ANGULAR
+  navigate(params) {  // FAKE IMP
+
+  }
+}
+
+class ActivatedRouteStub {
+  params: Observable<any> = Observable.empty();
+}
 describe('UserDetailsComponent', () => {
   let component: UserDetailsComponent;
   let fixture: ComponentFixture<UserDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserDetailsComponent ]
+      declarations: [UserDetailsComponent],
+      providers: [
+        { provide: Router, useClass: RouterStub },
+        { provide: ActivatedRoute, useClass: ActivatedRouteStub }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
