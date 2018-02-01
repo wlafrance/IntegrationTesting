3dee22b5c3b4c9afdd71512ea563f9b805dad156
Warren LaFrance <warrenlafrance@outlook.com>
2018-02-01 11:21:04 -0600

    In greeter.comp spec ts I commented out extrra code, no need to normally do this. Just a FYI

--- integration-testing-demo/src/app/app.component.spec.ts
+++ integration-testing-demo/src/app/app.component.spec.ts
@@ -24,16 +24,11 @@ describe('AppComponent', () => {
     fixture.detectChanges(); 
   });
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  // it('should create the app', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app).toBeTruthy();
+  // }));
   // it('should render title in a h1 tag', async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
--- integration-testing-demo/src/app/greeter/greeter.component.spec.ts
+++ integration-testing-demo/src/app/greeter/greeter.component.spec.ts
@@ -6,16 +6,19 @@ describe('GreeterComponent', () => {
   let component: GreeterComponent;
   let fixture: ComponentFixture<GreeterComponent>;
 
-  //Since we are using webpack we do not need to compile, it is done
-  //by ng cli be default - no need for ng cli
-  beforeEach(async(() => {
+  // Since we are using webpack we do not need to compile, it is done
+  //by ng cli be default - no need for ng cli does the compile for us
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     declarations: [ GreeterComponent ]
+  //   })
+  //   .compileComponents();
+  // }));
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ GreeterComponent ]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(GreeterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
