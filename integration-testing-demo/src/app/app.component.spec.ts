750eb91d90405de155d2e86d1fbea71b45061c36
wlafrance <warren.lafrance@gmail.com>
2018-02-02 07:05:48 -0600

    ROUTER LINK DIRECTIVE TEST ADDED

--- integration-testing-demo/src/app/app.component.spec.ts
+++ integration-testing-demo/src/app/app.component.spec.ts
@@ -4,6 +4,8 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterOutlet,RouterLinkWithHref } from '@angular/router';
+
 
 
 describe('AppComponent', () => {
@@ -12,7 +14,8 @@ describe('AppComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
+        imports: [RouterTestingModule.withRoutes([])],
+        declarations: [
         AppComponent
       ],
     }).compileComponents();
@@ -24,6 +27,25 @@ describe('AppComponent', () => {
     fixture.detectChanges(); 
   });
 
+  it('should have a router outlet',()=>{
+      let de = fixture.debugElement.query(By.directive(RouterOutlet));
+
+      expect(de).not.toBe(null);
+
+  });
+
+  // This technigue is good for "any" component that uses the routerLink directive
+  it('should have a link that points to the todo page',()=>{
+     // Because query will return the first, we actually need to get 
+     // an array of links.
+    //fixture.debugElement.query(By.directive(RouterLinkWithHref));
+    let debugElements = fixture.debugElement.queryAll(By.directive(RouterLinkWithHref));
+    
+    // <a href='/todos'>
+    let index = debugElements.findIndex(de=> de.properties['href']=== '/todos');
+    debugger;
+    expect(index).toEqual(1);
+  });
   // it('should create the app', async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   const app = fixture.debugElement.componentInstance;
